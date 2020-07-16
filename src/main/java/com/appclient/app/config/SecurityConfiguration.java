541daa0f7f6105ee36af6e70d27016d676a92d34
maximilianoPizarro <maximiliano.pizarro.5@gmail.com>
2020-07-16 11:35:39 -0700

    startup

---
+++ src/main/java/com/appclient/app/config/SecurityConfiguration.java
@@ -0,0 +1,116 @@
+package com.appclient.app.config;
+
+import com.appclient.app.security.*;
+
+import io.github.jhipster.config.JHipsterProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import com.appclient.app.security.oauth2.AudienceValidator;
+import com.appclient.app.security.SecurityUtils;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+
+import java.util.*;
+import com.appclient.app.security.oauth2.JwtGrantedAuthorityConverter;
+
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+@EnableWebSecurity
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Import(SecurityProblemSupport.class)
+public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+
+    private final JHipsterProperties jHipsterProperties;
+    private final SecurityProblemSupport problemSupport;
+
+    public SecurityConfiguration(JHipsterProperties jHipsterProperties, SecurityProblemSupport problemSupport) {
+        this.problemSupport = problemSupport;
+        this.jHipsterProperties = jHipsterProperties;
+    }
+       /**
+        * Registers the KeycloakAuthenticationProvider with the authentication manager.
+        */
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) {
+            SimpleAuthorityMapper grantedAuthorityMapper = new SimpleAuthorityMapper();
+            grantedAuthorityMapper.setPrefix("ROLE_");
+            KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+            keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(grantedAuthorityMapper);
+            auth.authenticationProvider(keycloakAuthenticationProvider);
+        }
+
+        @Bean
+        @Override
+        @ConditionalOnMissingBean(HttpSessionManager.class)
+        protected HttpSessionManager httpSessionManager() {
+            return new HttpSessionManager();
+        }
+       /**
+        * Defines the session authentication strategy.
+        */
+        @Bean
+        @Override
+        protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+            return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            super.configure(http);
+            http
+                    .csrf()
+                    .disable()
+                    .exceptionHandling()
+                        .authenticationEntryPoint(problemSupport)
+                        .accessDeniedHandler(problemSupport)
+                .and()
+                    .headers()
+                    .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
+                .and()
+                    .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
+                .and()
+                    .featurePolicy("geolocation 'none'; midi 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'none'; fullscreen 'self'; payment 'none'")
+                .and()
+                    .frameOptions()
+                    .deny()
+                .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/auth-info").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/management/health").permitAll()
+                .antMatchers("/management/info").permitAll()
+                .antMatchers("/management/prometheus").permitAll()
+                .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN);
+        }
+    }
---
+++ src/main/java/com/appclient/app/web/rest/DocumentoResource.java
@@ -0,0 +1,78 @@
+package com.appclient.app.web.rest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.util.Base64;
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.appclient.app.security.AuthoritiesConstants;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import io.github.jhipster.web.util.ResponseUtil;
+/**
+ * REST controller for managing Documento.
+ */
+@RestController
+@RequestMapping("/api")
+public class DocumentoResource {
+
+	private final Logger log = LoggerFactory.getLogger(DocumentoResource.class);
+
+	public DocumentoResource() {
+	}
+
+	/**
+	 * GET /private
+	 */
+	@RolesAllowed({ AuthoritiesConstants.ADMIN})
+	@GetMapping("/private")
+	public ResponseEntity<String>  ejemplo1() {
+		return ResponseEntity.ok().body("Admin full");
+	}
+
+
+    	/**
+	 * GET /private
+	 */
+	@RolesAllowed({ AuthoritiesConstants.ADMIN,AuthoritiesConstants.USER})
+	@PutMapping("/private")
+	public ResponseEntity<String>  ejemplo2() {
+		return ResponseEntity.ok().body("User");
+	}
+
+}
